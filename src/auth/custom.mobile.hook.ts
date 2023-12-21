733dd059e123d9634bda61846a482478dbe9be37
Misho <chapidze.misho95@gmail.com>
2023-12-21 21:17:59 +0400

    fix

--- src/auth/custom.mobile.hook.ts
+++ src/auth/custom.mobile.hook.ts
@@ -1,4 +1,7 @@
 export const useCustomMobileNumber = (mobile: string) => {
+  if (!mobile) {
+    return { mobileWithIndex: null, mobileWithOutIndex: null };
+  }
   const mobileWithIndex = mobile.includes('+995') ? mobile : `+995${mobile}`;
   const mobileWithOutIndex = mobile.includes('+995')
     ? mobile.split('+995')[1]
