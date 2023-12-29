f09bc1119e7d721a70ff5f809aafe5c0d7efcc6b
Misho <chapidze.misho95@gmail.com>
2023-12-30 00:45:56 +0400

    s

--- src/auth/cookie.options.ts
+++ src/auth/cookie.options.ts
@@ -1,13 +1,13 @@
 export const cookieOptionsToken = {
   httpOnly: true,
-  secure: true,
+  secure: false,
   sameSite: 'strict',
   expires: new Date(new Date().getTime() + 320 * 1000),
 };
 
 export const cookieOptionsTokenRefresh = {
   httpOnly: true,
-  secure: true,
+  secure: false,
   sameSite: 'strict',
   expires: new Date(new Date().getTime() + 3600 * 24 * 1000),
 };
